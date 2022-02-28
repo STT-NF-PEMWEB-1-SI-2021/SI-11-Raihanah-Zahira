95dcdcb36be4c09423fc0d46c5dacc014cb8eb9a
raihanazhr <91996001+raihanazhr@users.noreply.github.com>
2022-02-28 11:03:17 +0700

    Add files via upload

---
+++ Praktikum 1 PHP/variable_user.php
@@ -0,0 +1,14 @@
+<?php
+    //definisikan variable
+    $nama = 'Rosalie Naurah';
+    $umur = 13;
+    $berat = 22.4;
+
+    echo 'Nama :' .$nama;
+    echo '<br/>Umur :' .$umur. 'Tahun';
+    echo '<br/>Berat :' .$berat. 'Kg';
+
+    echo "<br/> Hello $nama apakabar";
+    echo "<br/> Hai nama saya $nama Umur saya $umur Berat badan saya $berat Kg, Salam kenal semuanya";
+    echo "<hr>";
+?>
